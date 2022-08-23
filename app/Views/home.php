07d136b418ba24776762121e5183bb04df290a3c
fazarrr <fazarbahari25@gmail.com>
2022-08-23 23:30:37 +0700

    update

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Dokumentasi</title>
-    <meta name="description" content="The small framework with powerful features">
+    <meta name="description" content="API Sekolah di Bekasi">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
 
